7d2dd7ddc0a2a6df52481d12b20098eac0324315
wicebing <wicebing@gmail.com>
2025-03-06 13:04:32 +0800

    mm

--- ntuha_step2_plot_position.py
+++ ntuha_step2_plot_position.py
@@ -177,5 +177,5 @@ with open("../databank/pkl/filter01.pkl", 'rb') as f:
 # with open("./guider20240808/databank/pkl/KalmanSmooth01.pkl", 'rb') as f:
 #     txyzPds_smooth = pickle.load(f)
 
-Trajectory_plot(events, txyzPds,1,'filter_0',grid=False)       
+Trajectory_plot(events[90:], txyzPds,1,'filter_0',grid=False)       
 Trajectory_plot(events, txyzPds_origin,1,'',grid=False)      
