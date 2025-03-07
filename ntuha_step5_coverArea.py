d09b2c0dea5a3b8e8f2c247ee886a726ef66c71f
wicebing <wicebing@gmail.com>
2025-03-07 13:37:07 +0800

    dd

--- ntuha_step5_coverArea.py
+++ ntuha_step5_coverArea.py
@@ -31,7 +31,7 @@ grid_size = 45
 all_area_coords = set()
 for i in range(25):   # grid_x <= 7 (0-7)
     for j in range(25):  # grid_y >= 16
-        all_area_coords.add((i, j))
+        all_area_coords.add((j, i))
 remove_coords = set()
 for i in range(25):  # Or range(25) if your grid is actually 0-24
     remove_coords.add((0, i))     # grid_x = 0
@@ -41,16 +41,13 @@ for j in range(25):  # Or range(25) if your grid is actually 0-24
     remove_coords.add((j, 24))    # grid_y = 24
 for i in range(8):   # grid_x <= 7 (0-7)
     for j in range(16, 25):  # grid_y >= 16
-        remove_coords.add((i, j))
+        remove_coords.add((j, i))
 for i in range(3):   # grid_x <= 7 (0-7)
     for j in range(13, 16):  # grid_y >= 16
-        remove_coords.add((i, j))
-for i in range(3):   # grid_x <= 7 (0-7)
-    for j in range(13, 16):  # grid_y >= 16
-        remove_coords.add((i, j))
-for i in range(4,18):   # grid_x <= 7 (0-7)
+        remove_coords.add((j, i))
+for i in range(4,17):   # grid_x <= 7 (0-7)
     for j in range(5):  # grid_y >= 16
-        remove_coords.add((i, j))
+        remove_coords.add((j, i))
 all_area_coords = all_area_coords-remove_coords
 
 def preprocess_1(df, time_col='positionTime'):
@@ -175,6 +172,56 @@ def plot_coords(aa2, grid=False):
 
 # plot_coords(aa2)
 
+# i want to plot all_area_coords on the background map to check it
+def plot_all_area_coords():
+    x_min=302491 - 302491
+    x_max=302516 - 302491
+    y_min=2770397 - 2770397
+    y_max=2770422 - 2770397
+    scale = 45
+    grid_size = 45
+
+    # Load the image
+    img = Image.open('../databank/ED_Area.png')
+    img_array = np.array(img)
+    img_array = np.flipud(img_array)
+
+    heatmap_rows = img_array.shape[0] // grid_size
+    heatmap_cols = img_array.shape[1] // grid_size
+
+    fig, ax = plt.subplots(figsize=(10, 10))  # adjust figsize for better view
+    ax.imshow(img_array)
+
+    # Draw coordinates in all_area_coords
+    for coord in all_area_coords:
+        x, y = coord
+        rect = mpatches.Rectangle((y * grid_size, x * grid_size), grid_size, grid_size,
+                                  alpha=0.5, facecolor='yellow', edgecolor='yellow')
+        ax.add_patch(rect)
+
+    # Set plot limits
+    major_ticks = np.arange(0, 1125, grid_size)
+    ax.grid(which='major', alpha=0.5, linestyle='--')
+    ax.set_xticks(major_ticks)
+    ax.set_yticks(major_ticks)
+    plt.xticks([])
+    plt.yticks([])
+
+    ax.set_xlim(0, scale * (x_max - x_min))
+    ax.set_ylim(0, scale * (y_max - y_min))
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title(f'Position heatmap_all_area_coords')
+
+    pic_filepath = f'../output/all_area_coords.png'
+    os.makedirs(os.path.dirname(pic_filepath), exist_ok=True)
+
+    plt.savefig(fname=pic_filepath)
+    print(f' === complete all_area_coords image === ')
+
+plot_all_area_coords()
+
 aa3 = aa2.copy()
 aa3.loc[:,['axis_agg']] = aa3['axis_agg'].apply(lambda x: x - remove_coords)
 aa3['corrd_number'] = aa3['axis_agg'].apply(len)
@@ -188,7 +235,7 @@ akk = akk.reset_index()
 akk = akk.pivot(columns='weekday', index='hour')
 akk.to_csv('../output/areaPct_report_bydayhour.csv')
 
-
+# plot_coords(aa3)
 
 # Load the event timePoint
 events = pd.read_excel("../databank/events_2025_d.xlsx")
