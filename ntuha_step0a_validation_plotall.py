bb369c7a93590126718a1a59d8d7434bb377d615
wicebing <wicebing@gmail.com>
2025-03-13 11:32:45 +0800

    mm

---
+++ ntuha_step0a_validation_plotall.py
@@ -0,0 +1,209 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+import datetime,os,math, pytz, json, pickle
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.colors as mcolors
+from matplotlib.colors import to_rgb, to_rgba
+
+import utils
+
+databank_filepath = "../databank/positions_VAL"
+os.makedirs(databank_filepath,exist_ok=True)
+
+local_timezone = pytz.timezone('Asia/Taipei') 
+ 
+select_beacons =['N002', 'N015', 'N016', 'N031']
+beacon_ids = select_beacons #utils.get_beacons()
+print('=== load beacons ids ===')
+
+x_min=302491
+x_max=302516
+y_min=2770397
+y_max=2770422
+scale = 45
+grid_size = 45
+txyzPds = {}
+
+for beacon in beacon_ids:
+    recordName= f'{beacon}.pkl'
+    pickle_filepath = os.path.join(databank_filepath,recordName)
+    
+    if os.path.isfile(pickle_filepath):
+        print(f'=== {beacon}.pkl exist, loading ===')
+
+        txyzPd_origin = pd.read_pickle(pickle_filepath)
+        pd_pz = pd.json_normalize(txyzPd_origin['position'])
+        pd_time = pd.to_datetime(txyzPd_origin['positionTime'],format='mixed').dt.tz_convert(local_timezone)
+        df = pd.concat([pd_time,pd_pz],axis=1)
+        
+        df['x'] = df['x']-x_min
+        df['y'] = df['y']-y_min
+        
+        txyzPds[beacon]=df
+
+def plot_trajectory(dfs, pic_name='evtTimePoint',grid=False, evt=0):
+    """Plots the trajectory of points from a DataFrame.
+
+    Args:
+        df: Pandas DataFrame with 'positionTime' (datetime) and 'position' (dict).
+    """
+    x_min=302491 - 302491
+    x_max=302516 - 302491
+    y_min=2770397 - 2770397
+    y_max=2770422 - 2770397
+    scale = 45
+    grid_size = 45
+    
+    fig, ax = plt.subplots(figsize=(10, 10))  # adjust figsize for better view
+    # Load the image
+    img = Image.open('../databank/ED_Area.png')
+    img_array = np.array(img)
+    img_array = np.flipud(img_array)     
+    ax.imshow(img_array)
+    
+    colors = {'N002':'blue',
+              'N015':'violet',
+              'N016':'limegreen',
+              'N031':'darkorange',
+              'N006':'tomato',
+              'N007':'royalblue',
+              'N008':'peru',
+              'N017':'salmon',
+              'N029':'peru',
+              'N030':'blue'}
+
+    if evt == 0:
+        # i want to plot the ground turth route on the map
+        # y=6 start from x=5 to x=20.5 then 
+        # x=20.5  from y=6 to y=23.5 then
+        # y=23.5 from x=20.5 to x= 12.5 then
+        # x=12.5 from y=23.5 to y=8 then
+        # y=8 from x=12.5 to x=5, start plot the route
+        x = [5,20.5,20.5,12.5,12.5,5]
+        y = [6,6,23.5,23.5,8,8]
+        x = scale*np.array(x)
+        y = scale*np.array(y)
+        ax.plot(x, y, color='red', alpha = 0.5, linestyle = '-',linewidth=10)
+    elif evt==1:
+        # i want to plot the ground turth route on the map
+        # x=4  from y=9 to y=14.5 then
+        # y=14.5 from x=4 to x= 9 
+        # start plot the route
+        x = [4,4,10]
+        y = [9,14.5,14.5]
+        x = scale*np.array(x)
+        y = scale*np.array(y)
+        ax.plot(x, y, color='red', alpha = 0.5, linestyle = '-',linewidth=10)
+    elif evt==-1:
+        x = [5,20.5,20.5,12.5,12.5,4,4,10]
+        y = [6,6,23.5,23.5,8,8,14.5,14.5]
+        x = scale*np.array(x)
+        y = scale*np.array(y)
+        ax.plot(x, y, color='red', alpha = 0.5, linestyle = '-',linewidth=10)
+
+    total_points = 0
+    for k,d in dfs.items():
+        df = d.copy()
+        total_points += len(df)
+        # Extract x and y coordinates; handle potential errors.
+        x = scale*(df['x']-x_min)
+        y = scale*(df['y']-y_min)
+        
+        df['time_diff'] = df['positionTime'].diff().dt.total_seconds()
+        
+        times = df['positionTime'].values
+        
+        # Calculate alpha values for transparency (optional)
+        alpha_values = np.linspace(0.0, 0.75, len(x)) # Adjust transparency as needed
+        r, g, b = to_rgb(colors[k])
+        clr = [(r, g, b, alpha) for alpha in np.floor(alpha_values*10)/10]
+        
+        # Plot points with transparency
+        ax.scatter(x, y, c = clr, alpha=0.5, s = 15)
+
+        # Add a line connecting the points
+        consecutive_indices = np.where(df['time_diff'].values > 10)
+        if consecutive_indices[0].size > 1:
+            cons_i = 0
+            for i, idx in enumerate(consecutive_indices[0]):
+                if i == consecutive_indices[0].size-1:
+                    x_consecutive = x[cons_i:]
+                    y_consecutive = y[cons_i:]
+                else:
+                    x_consecutive = x[cons_i:idx]
+                    y_consecutive = y[cons_i:idx]
+                    cons_i=idx                
+                ax.plot(x_consecutive, y_consecutive, color=colors[k], alpha = 0.1, linestyle = '-')
+        else:
+            ax.plot(x, y, color=colors[k], alpha = 0.1, linestyle = '-') 
+
+    # Set plot limits
+    major_ticks = np.arange(0, 25, 1)
+    ax.grid(which='major', alpha=0.5, linestyle='--')
+    ax.set_xticks(major_ticks * grid_size)
+    ax.set_yticks(major_ticks * grid_size)
+    ax.set_xticklabels(major_ticks)
+    ax.set_yticklabels(major_ticks)
+    if(not grid):
+        plt.xticks([])
+        plt.yticks([])
+    
+    ax.set_xlim(0,scale*(x_max-x_min))
+    ax.set_ylim(0,scale*(y_max-y_min))
+    
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title(f'Position Trajectory_{pic_name}_{total_points}points')
+
+    # plt.axis('off')
+    plt.grid(True)
+    filename = f'../output/val/{pic_name}.png'
+    os.makedirs(os.path.dirname(filename),exist_ok=True)
+
+    plt.savefig(fname=filename)
+    print(f' === complete {pic_name} image === ')
+
+# Draw Trajectory 
+def Trajectory_plot(events, drawPds,mins=1,flag='origin',grid=False):
+    for i, evt in events.iterrows():
+        print(f' == work on {i} event == ')
+        endtime = evt['endTime']
+        startTime = evt['startTime']
+        
+        # if i != 30:
+        #     continue
+        dfs = {}
+        for beacon in select_beacons:
+            df = drawPds[beacon].loc[(drawPds[beacon]['positionTime'] >= startTime) & (drawPds[beacon]['positionTime'] <= endtime)]    
+            if len(df) > 0:
+                dfs[beacon]=df
+        
+        plot_trajectory(dfs, pic_name=f'{i+1}_{endtime.hour}{endtime.minute}', grid=grid, evt=i)
+
+def plot_all_trajectories(events, drawPds, grid=False):
+    """Plots all events on a single map."""
+    dfs = {}
+    for i, evt in events.iterrows():
+        print(f' == work on {i} event == ')
+        endtime = evt['endTime']
+        startTime = evt['startTime']
+        
+        for beacon in select_beacons:
+            df = drawPds[beacon].loc[(drawPds[beacon]['positionTime'] >= startTime) & (drawPds[beacon]['positionTime'] <= endtime)]
+            if beacon not in dfs:
+                dfs[beacon] = df
+            else:
+                dfs[beacon] = pd.concat([dfs[beacon], df])
+    
+    plot_trajectory(dfs, pic_name='all_events', grid=grid, evt=-1)
+
+# Load the event timePoint
+events = pd.read_excel("../databank/events_val_route.xlsx",dtype={'日期':str,'開始時間':str,'結束時間':str})
+# events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y%m%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
+events['startTime'] = pd.to_datetime(events['日期'] + ' ' + events['開始時間'], format='%Y%m%d %H%M%S', errors='coerce').dt.tz_localize(local_timezone)
+events['endTime'] = pd.to_datetime(events['日期'] + ' ' + events['結束時間'], format='%Y%m%d %H%M%S', errors='coerce').dt.tz_localize(local_timezone)
+
+# Plot all events on a single map
+plot_all_trajectories(events[:2], txyzPds, grid=True)
--- ntuha_step6_count_nurse_loading.py
+++ ntuha_step6_count_nurse_loading.py
@@ -5,18 +5,17 @@ import datetime,os,math, pytz, json, pickle
 from functools import reduce
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import matplotlib.patches as mpatches
 import matplotlib.colors as mcolors
 from matplotlib.colors import to_rgb, to_rgba
 
 import utils
 
-databank_filepath = "../databank/positions"
+databank_filepath = "./guider20240808/databank/positions"
 os.makedirs(databank_filepath,exist_ok=True)
 
 local_timezone = pytz.timezone('Asia/Taipei') 
  
-select_beacons =['N002', 'N003', 'N004', 'N005', 'N006', 'N007', 'N008', 'N017', 'N029', 'N030']
+select_beacons =['N002', 'N003', 'N004', 'N005', 'N006', 'N007', 'N008', 'N017', 'N029']
 beacon_ids = select_beacons #utils.get_beacons()
 print('=== load beacons ids ===')
 
@@ -27,8 +26,8 @@ y_max=2770422
 scale = 45
 grid_size = 45
 
-def preprocess_1(df, time_col='positionTime'):
-    dfc = df.sort_values(by=time_col).copy()
+def filter_single(df, time_col='positionTime'):
+    dfc = df.copy().sort_values(by=time_col)
 
     # Calculate differences in position and time (in seconds)
     dfc['x_diff'] = dfc['x'].diff()
@@ -50,24 +49,31 @@ def preprocess_1(df, time_col='positionTime'):
     dfc['hour'] = dfc[time_col].dt.hour
     
     dfc['loss_tick'] = np.maximum(np.floor(dfc['time_diff'] - 1),0).fillna(0)
-    dfc['id_mins'] = dfc[time_col].dt.round('min')
+    dfc['id_hours'] = dfc['positionTime'].dt.round('min')
     
     temp = dfc[dfc['skip_change']>0]
     dfc.loc[temp.index,'time_diff']=0
     dfc.loc[temp.index,'position_diff']=0
-
+    
     return dfc
 
+# Load the event timePoint
+events = pd.read_excel("./guider20240808/databank/events.xlsx")
+events['日期'] = events['日期'].astype(str)
+events['時間'] = events['時間'].astype(str)
+events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y-%m-%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
+events = events[['positionTime','發生地點','事件分類', 'X', 'Y']]
+
 # Load the beacon positionTime
-with open("../databank/pkl/filter02_dt.pkl", 'rb') as f:
-    txyzPds = pickle.load(f)
+with open("./guider20240808/databank/pkl/filter01.pkl", 'rb') as f:
+    txyzPds = pickle.load(f) 
 
 aao = txyzPds.copy()
 aa={}
 lossTick = {}
 for k,v in aao.items():
-    aa[k] = preprocess_1(v)
-
+    aa[k] = filter_single(v)
+    
 N029 = aa['N029'].set_index('positionTime')
 N008 = aa['N008'].set_index('positionTime')
 
@@ -75,28 +81,19 @@ N008new = pd.concat([N008[:"2024-10-17 09:00:00"],N029["2024-10-17 09:01:00":]],
                     axis=0,
                     ignore_index=False)
 aa.pop('N029')
-aa['N008'] = N008new.reset_index()
-
-N002 = aa['N002'].set_index('positionTime')
-N030 = aa['N030'].set_index('positionTime')
-
-N002new = pd.concat([N002[:"2025-01-20 08:00:00"],N030["2025-01-20 08:01:00":]],
-                    axis=0,
-                    ignore_index=False)
-aa.pop('N030')
-aa['N002'] = N002new.reset_index()
+aa['N008'] = filter_single(N008new.reset_index())
 
 loadings = []
 for k, v in aa.items():
-    res = v.groupby(['id_mins', 'skip']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
-    load = res.reset_index().groupby(['id_mins']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
+    res = v.groupby(['id_hours', 'skip']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
+    load = res.reset_index().groupby(['id_hours']).agg({'time_diff': 'sum', 'position_diff': 'sum'})
     load['mps'] = load['position_diff']/load['time_diff']
     load = load.rename(columns={'time_diff': f'time_diff_{k}', 
                                 'position_diff': f'position_diff_{k}',
                                 'mps': f'mps_{k}',})
     loadings.append(load.reset_index())
 
-load_all = reduce(lambda x, y: x.merge(y, how='outer', on='id_mins'), loadings)
+load_all = reduce(lambda x, y: x.merge(y, how='outer', on='id_hours'), loadings)
 # Sum time_diff columns (after renaming)
 time_diff_cols = [col for col in load_all.columns if col.startswith('time_diff_')]
 load_all['time_diff_all'] = load_all[time_diff_cols].sum(axis=1)
@@ -104,21 +101,21 @@ position_diff_cols = [col for col in load_all.columns if col.startswith('positio
 load_all['position_diff_all'] = load_all[position_diff_cols].sum(axis=1)
 load_all['mps_all'] = -1*load_all['position_diff_all']/load_all['time_diff_all']
 
-load_all = load_all.set_index('id_mins')
+load_all['event'] = 0
+load_all = load_all.set_index('id_hours')
 
 mps_cols = [col for col in load_all.columns if col.startswith('mps_')]
 plot_data = load_all[mps_cols].copy()
 
 
 # Load the event timePoint
-events = pd.read_excel("../databank/events_2025_d.xlsx",dtype={'日期':str,'時間':str})
-events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y%m%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
+events = pd.read_excel("./guider20240808/databank/events.xlsx")
+events['日期'] = events['日期'].astype(str)
+events['時間'] = events['時間'].astype(str)
+events['positionTime'] = pd.to_datetime(events['日期'] + ' ' + events['時間'], format='%Y-%m-%d %H%M', errors='coerce').dt.tz_localize(local_timezone)
 events = events[['positionTime','發生地點','事件分類', 'X', 'Y']]
 
-before_event_minutes = 60
-
-plot_data['event_f'] = 0
-plot_data['event_c'] = 0
+plot_data['event'] = 0
 for i, evt in events.iterrows():
     print(f' == work on {i} event == ')
     positionTime = evt['positionTime']
@@ -127,39 +124,40 @@ for i, evt in events.iterrows():
     evt_what = evt['事件分類']
     發生地點 = evt['發生地點']
     endtime = positionTime-datetime.timedelta(minutes=5)
-    startTime = endtime-datetime.timedelta(minutes=before_event_minutes)
+    startTime = endtime-datetime.timedelta(hours=1)
     
-    # fig, ax = plt.subplots(figsize=(20, 10))  # adjust figsize for better view
-    # x_consecutive = plot_data.loc[startTime:endtime][mps_cols]
-    # ax = x_consecutive.plot(figsize=(30,10),ylim=(-0.5,0.5))
-    # plt.savefig(fname=f'./output/loading/{i}_{evt_what}.png')
+    fig, ax = plt.subplots(figsize=(20, 10))  # adjust figsize for better view
+    
+    x_consecutive = plot_data.loc[startTime:endtime][mps_cols]
+    
+    ax = x_consecutive.plot(figsize=(30,10),ylim=(-0.5,0.5))
+    plt.savefig(fname=f'./output/loading/{i}_{evt_what}.png')
 
-    plot_data.loc[startTime:endtime,['event_c']] = 1 if evt_what=='轉重症' else 0
-    plot_data.loc[startTime:endtime,['event_f']] = 1 if evt_what=='跌倒' else 0
+    plot_data.loc[startTime:endtime,['event']] = 1 if evt_what=='轉重症' else 2
 
-jjjc = plot_data.groupby('event_c').agg({k: ['mean','std'] for k in mps_cols})
-jjjc.to_csv('../output/loadingC_report_byCart.csv')
+plot_data['event'] = plot_data['event'].fillna(0)
 
-jjjf = plot_data.groupby('event_f').agg({k: ['mean','std'] for k in mps_cols})
-jjjf.to_csv('../output/loadingF_report_byCart.csv')
+jjj = plot_data.groupby('event').agg({k: ['mean','std'] for k in mps_cols})
+jjj.to_csv('./output/loading_report_byCart.csv')
 
 ana = plot_data.reset_index().copy()
-ana['weekday'] = ana['id_mins'].dt.weekday
-ana['hour'] = ana['id_mins'].dt.hour
+ana['weekday'] = ana['id_hours'].dt.weekday
+ana['hour'] = ana['id_hours'].dt.hour
 analysis_ = [] 
 for k in mps_cols:
     if k == 'mps_all': continue
-    temp = ana.loc[:,['id_mins',k,'event_c','event_f','weekday','hour']]
-    temp.columns = ['id_mins','mps','event_c','event_f','weekday','hour']
-    analysis_.append(temp.dropna())
+    temp = ana.loc[:,['id_hours',k,'event','weekday','hour']]
+    temp.columns = ['id_hours','mps','event','weekday','hour']
+    analysis_.append(temp)
 
 analysis = pd.concat(analysis_, axis=0, ignore_index=True)
-# jjj2 = analysis.groupby('event').agg({'mps': ['mean','std']})
-# print(jjj2)
+jjj2 = analysis.groupby('event').agg({'mps': ['mean','std']})
+print(jjj2)
 
-analysis.dropna().to_csv('../output/analysis/loading_event.csv')
+analysis.dropna().to_csv('./analysis/loading_event.csv')
+
 
 akk = analysis.groupby(['weekday','hour']).agg({'mps': ['mean','std']})
 akk = akk.reset_index()
 akk = akk.pivot(columns='weekday', index='hour')
-akk.to_csv('../output/loading_report_bydayhour.csv')
+akk.to_csv('./output/loading_report_bydayhour.csv')
